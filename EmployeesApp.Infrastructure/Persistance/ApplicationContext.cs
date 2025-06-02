a16613ef860e1ad38f6284fa4e5e93314fad3db5
Nowaxial <Nowaxial@users.noreply.github.com>
2025-06-02 13:29:26 +0200

    Add Entity Framework Core support and configure DB context
    Updated project files to include Entity Framework Core packages.
    Modified Program.cs to set up dependency injection and
    retrieve the connection string. Added connection string to
    appsettings.json. Created ApplicationContext.cs to define
    the database context and seed sample Employee data.

---
+++ EmployeesApp.Infrastructure/Persistance/ApplicationContext.cs
@@ -0,0 +1,62 @@
+﻿using EmployeesApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Infrastructure.Persistance
+{
+    public class ApplicationContext(DbContextOptions<ApplicationContext> options)
+: DbContext(options)
+    {
+        // Exponerar våra entiteter via properties av typen DbSet<T>
+        public DbSet<Employee> Customers { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Anropen nedan är exempel på att använda Fluent API (till skillnad från att
+            // använda attribut direkt på entiteterna)
+            // Specificerar vilken datatyp databasen ska använda för en specifik kolumn
+            modelBuilder.Entity<Employee>();
+
+            // Specificerar data som en specific tabell ska för-populeras med
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee()
+                {
+                    Id = 562,
+                    Name = "Anders Hejlsberg",
+                    Email = "Anders.Hejlsberg@outlook.com",
+                },
+        new Employee()
+        {
+            Id = 62,
+            Name = "Kathleen Dollard",
+            Email = "k.d@outlook.com",
+        },
+        new Employee()
+        {
+            Id = 15662,
+            Name = "Mads Torgersen",
+            Email = "Admin.Torgersen@outlook.com",
+        },
+        new Employee()
+        {
+            Id = 52,
+            Name = "Scott Hanselman",
+            Email = "s.h@outlook.com",
+        },
+        new Employee()
+        {
+            Id = 563,
+            Name = "Jon Skeet",
+            Email = "j.s@outlook.com",
+        });
+        }
+    }
+
+
+}
--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -1,11 +1,13 @@
-using EmployeesApp.Application.Employees.Interfaces;
-using EmployeesApp.Infrastructure.Persistance.Repositories;
+﻿using EmployeesApp.Application.Employees.Interfaces;
 using EmployeesApp.Application.Employees.Services;
+using EmployeesApp.Infrastructure.Persistance;
+using EmployeesApp.Infrastructure.Persistance.Repositories;
 using EmployeesApp.Web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeesApp.Web;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
@@ -14,6 +16,15 @@ public class Program
         builder.Services.AddSingleton<IEmployeeRepository, EmployeeRepository>();
         builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
         builder.Services.AddScoped<MyLogServiceFilterAttribute>();
+
+        // Hämta connection-strängen från AppSettings.json
+        var connString = builder.Configuration
+        .GetConnectionString("DefaultConnection");
+        // Registrera Context-klassen för dependency injection
+        builder.Services.AddDbContext<ApplicationContext>(
+        o => o.UseSqlServer(connString));
+
+
         var app = builder.Build();
         app.MapControllers();
         app.Run();
